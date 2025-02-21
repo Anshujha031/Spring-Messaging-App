f1e4fa690ac141366291c3da997251e47891d45f
naveenjha031 <naveenjha344@gmail.com>
2025-02-21 16:52:10 +0530

    [Naveen] Add: set up spring messaging app

---
+++ src/main/java/com/example/springlearningmessagingapp/SpringlearningmessagingappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlearningmessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningmessagingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearningmessagingappApplication.class, args);
+	}
+
+}
