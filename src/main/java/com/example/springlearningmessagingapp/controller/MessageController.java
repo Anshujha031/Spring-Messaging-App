caf0b1a2a30bdc6bb5c0c5ee0579023b2e271cf2
naveenjha031 <naveenjha344@gmail.com>
2025-02-21 21:12:59 +0530

    [Naveen] Add: Updated code from UC1

---
+++ src/main/java/com/example/springlearningmessagingapp/controller/MessageController.java
@@ -0,0 +1,33 @@
+package com.example.springlearningmessagingapp.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class MessageController {
+
+    // GET Method - Responds with "Hello from BridgeLabz"
+        @GetMapping
+        public String sayHello() {
+            return "Hello  BridgeLabz";
+        }
+
+        // POST Method - Accepts name and responds with personalized message
+        @PostMapping
+        public String sayHelloPost(@RequestBody String name) {
+            return "Hello, " + name + " from BridgeLabz!";
+        }
+
+        // PUT Method - Updates a message
+        @PutMapping("/{name}")
+        public String updateHello(@PathVariable String name) {
+            return "Updated Hello, " + name + " from BridgeLabz!";
+        }
+
+        // DELETE Method - Deletes a message
+        @DeleteMapping("/{name}")
+        public String deleteHello(@PathVariable String name) {
+            return "Goodbye, " + name + " from BridgeLabz!";
+        }
+    }
+
+
